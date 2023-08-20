52f739e4dbee6bb24ea55dd8a08628338a5d6b6a
mohamedlotfy <hammad.mohamed1893@gmail.com>
2023-08-20 15:13:02 +0200

    Initial Commit

---
+++ src/main/java/com/yumm/TargoApplication.java
@@ -0,0 +1,13 @@
+package com.yumm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TargoApplication.class, args);
+    }
+
+}
